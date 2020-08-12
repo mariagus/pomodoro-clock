807fb11b0dc64564a58fc6c16d125f360770006c
Maria Gusova <masha.gu@gmail.com>
2020-08-12 16:28:48 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,65 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
+      <header>POMODORO CLOCK</header>
+      <Timer />
+      <button id="reset">⟲</button>
+      <div className="timeSelector">
+        <p id="break-label">
+          BREAK LENGTH:
+          <BreakSetter />
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <p id="session-label">
+          SESSION LENGTH: <SessionSetter />
+        </p>
+      </div>
+    </div>
+  );
+}
+
+function Timer() {
+  const [sessionType, setSessionType] = useState("WORK!"); //state changes to "BREAK" when timer reaches zero.
+  const [min, setMin] = useState(25);
+  const [sec, setSec] = useState(0);
+
+  function handleStart() {}
+
+  return (
+    <div className="Timer">
+      <p id="sessionType">{sessionType}</p>
+      <h1>
+        {min < 10 ? `0${min}` : min}:{sec < 10 ? `0${sec}` : sec}
+      </h1>
+      <div className="timerButtons">
+        <button id="start" onClick={handleStart}>
+          ▷
+        </button>
+        <button id="pause">| |</button>
+      </div>
+    </div>
+  );
+}
+
+function BreakSetter() {
+  const [breakLength, setBreakLength] = useState(5);
+  return (
+    <div className="BreakSetter">
+      <button className="decrement">-</button>
+      {breakLength}
+      <button className="increment">+</button>
+    </div>
+  );
+}
+function SessionSetter() {
+  const [sessionLength, setSessionLength] = useState(25);
+  return (
+    <div className="SessionSetter">
+      <button className="decrement">-</button>
+      {sessionLength}
+      <button className="increment">+</button>
     </div>
   );
 }
