5f72f0a7b7b27ff27352e4685e29407d6d807bc9
Maria Gusova <masha.gu@gmail.com>
2020-08-15 18:03:15 +0200

    Updates

---
+++ precache-manifest.876aee1d4977a11d3a9ddbd82e1c5713.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c5b7a2e936aae2f30873bbdabe6664a6",
+    "url": "/pomodoro-clock/index.html"
+  },
+  {
+    "revision": "df7a66ab6929b9e9cccb",
+    "url": "/pomodoro-clock/static/css/main.7eb5e2a7.chunk.css"
+  },
+  {
+    "revision": "1062e676e1ee41b6fc38",
+    "url": "/pomodoro-clock/static/js/2.a3370f76.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/pomodoro-clock/static/js/2.a3370f76.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "df7a66ab6929b9e9cccb",
+    "url": "/pomodoro-clock/static/js/main.c767ebd1.chunk.js"
+  },
+  {
+    "revision": "d420e891230cef74bfe7",
+    "url": "/pomodoro-clock/static/js/runtime-main.cd73fa2f.js"
+  },
+  {
+    "revision": "f3e1419df2a1ac00aadeb8d1b62fe4c7",
+    "url": "/pomodoro-clock/static/media/bell.f3e1419d.mp3"
+  }
+]);
