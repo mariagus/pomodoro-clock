81e2e9833e7d944ae1befb608e47ad76ab294731
Maria Gusova <masha.gu@gmail.com>
2020-08-25 16:21:14 +0100

    Updates

--- precache-manifest.d8f757f74280af2a23024b284c197121.js
+++ precache-manifest.d8f757f74280af2a23024b284c197121.js
@@ -1,11 +1,11 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "7b3d868d89ef19962c2baf23df2b0996",
+    "revision": "468e98e1f56d37cf3da7182c91094c79",
     "url": "/pomodoro-clock/index.html"
   },
   {
-    "revision": "f63a2eb4415ebb77ba28",
-    "url": "/pomodoro-clock/static/css/main.988d8a89.chunk.css"
+    "revision": "dd49e83041b7b6df4d19",
+    "url": "/pomodoro-clock/static/css/main.60000f74.chunk.css"
   },
   {
     "revision": "1062e676e1ee41b6fc38",
@@ -16,7 +16,7 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/pomodoro-clock/static/js/2.a3370f76.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "f63a2eb4415ebb77ba28",
+    "revision": "dd49e83041b7b6df4d19",
     "url": "/pomodoro-clock/static/js/main.8d57363b.chunk.js"
   },
   {
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/pomodoro-clock/precache-manifest.128a809cf04ae16335d57e2e3145a8c3.js"
+  "/pomodoro-clock/precache-manifest.d8f757f74280af2a23024b284c197121.js"
 );
 
 self.addEventListener('message', (event) => {
